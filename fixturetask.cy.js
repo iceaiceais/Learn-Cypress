667feb98609de7fd14d42d21ab064ad559ae6fc9
iceaiceais <solehahnuraisyah@gmail.com>
2022-11-30 20:46:37 +0700

    updated fixture task

--- fixturetask.cy.js
+++ fixturetask.cy.js
@@ -1,29 +1,29 @@
+/// <reference types="cypress" />
 
 describe('Working with fixture and static data', () => {
   it('fixture trial', () => {
-
+    
       cy.visit('https://www.saucedemo.com/');
-      cy.fixture('user').then(user => {
-        const username = user.usernames
-        const password = user.password
-        
-        cy.get('#user-name').input('username')
       
-        cy.get('#password').input('password')
-        cy.get('#login-button').click()
+      cy.fixture('user').then(data => {
+        this.data = data;
       })
-
-      cy.fixture('user2').then(user => {
-        const username = user2.usernames
-        const password = user2.password
-        
-        cy.get('#user-name').input('username')
+      it('standar user' , function(){
+        cy.get('#user-name').type(this.data.username)
       
-        cy.get('#password').input('password')
+        cy.get('#password').type(this.data.password)
         cy.get('#login-button').click()
-      cy.get("error-button").should('contain', '"Epic sadface: Sorry, this user has been locked out."')
-   
 
-    });
+      })
+      
+        it('unmatch password', () => {})
+        cy.get('#user-name').type(this.data.username)
+      
+        cy.get('#password').type(this.data.unmatchpassword)
+        cy.get('#login-button').click()
+        
+        cy.get("error-button").should('contain', 'Epic sadface: Sorry, this user has been locked out.')
+   
+      
+    })
   })
-})
