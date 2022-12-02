7efca43e04d3627b8e40eefe08fab3e518608f4c
iceaiceais <solehahnuraisyah@gmail.com>
2022-12-03 00:50:34 +0700

    new login logout

--- LoginLogoutCase.cy.js
+++ LoginLogoutCase.cy.js
@@ -3,24 +3,25 @@
 describe('E2E Login Logout', () => {
     it('Login Logout test case', () => {
       
-       cy.visit('http://zero.webappsecurity.com/login.html/');
+       cy.visit('http://zero.webappsecurity.com/login.html');
        
-       cy.fixture('credentials').then(credentials => {
-      this.credentials = credentials;
+       cy.fixture('user').then(data => {
+        this.data = data;
       })
    
-    it('login', () => {})
-        cy.get('#user_login').type(this.credentials.username)
-        cy.get('#user_password').type(this.credentials.password)
+    it('login', () => {
+        cy.get('#user_login').type(this.data.username2)
+        cy.get('#user_password').type(this.data.password2)
         
         cy.get('#user_remember_me').check()
         cy.get('[name=submit]').click()
         cy.get('ul.nav-tabs').should('be.visible')
-
-    it('logout', () => {})
+    })
+    it('logout', () => {
         cy.contains('username').click()
         cy.get('#logout_link').click()
         cy.get('#homeMenu').should('contain.text', 'HOME')
   
       })    
+})
 })
